2736fffe74a30988df9b8d6ecc11ec6e34d5b43d
Vinay Prakash Yadav <vinayprakashyadav8364@gmail.com>
2024-12-29 14:22:54 +0530

    java script basics

---
+++ Test.js
@@ -0,0 +1,12 @@
+// let list=[1,2,3,4,5]
+// let sum=0;
+// for(let i=0;i<list.length;i++){
+//     console.log(i)
+//    sum+=list[i]
+// }
+// console.log(sum)
+// console.log(typeof sum)
+// console.log(typeof list)
+
+let userInput = prompt("Please enter your age:");
+console.log(userInput);
